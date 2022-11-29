535e42b390ee190c119160d02d9f2ef3d61e6bb5
Chad Mcbride <chadmcbride.cb@gmail.com>
2022-11-30 01:10:49 +0200

    Adding-Files

---
+++ C##/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace C__
+{// the problem lies after the second string variable is used.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string misc;
+            string cisco;
+            Console.WriteLine("How to concatenante");
+           misc = Console.ReadLine();
+           Console.WriteLine("Enter another variable");
+          // cisco = Console.WriteLine();
+           // Console.WriteLine(misc + cisco);
+        }
+    }
+}
